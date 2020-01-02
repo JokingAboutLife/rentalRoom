27741bb04968730de809394664928583c624dc03
shicaihua <2502993659@qq.com>
2020-01-02 20:58:04 +0800

    毕设项目

---
+++ rental_room/src/test/java/com/gxust/edu/rental_room/RentalRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxust.edu.rental_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
