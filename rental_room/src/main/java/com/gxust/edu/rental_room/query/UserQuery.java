3ed2a6964da620454a3ae172d948aec7ef803d67
shicaihua <2502993659@qq.com>
2020-01-07 22:24:07 +0800

    用戶管理

---
+++ rental_room/src/main/java/com/gxust/edu/rental_room/query/UserQuery.java
@@ -0,0 +1,22 @@
+package com.gxust.edu.rental_room.query;
+
+public class UserQuery {
+    private String loginName;
+    private String name;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
