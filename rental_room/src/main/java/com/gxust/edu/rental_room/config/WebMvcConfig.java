b6190fccd12efdf4e1a4c204bd5aeda808c9bb20
shicaihua <2502993659@qq.com>
2020-01-13 11:46:22 +0800

    解决跨域请求问题

--- rental_room/src/main/java/com/gxust/edu/rental_room/config/WebMvcConfig.java
+++ rental_room/src/main/java/com/gxust/edu/rental_room/config/WebMvcConfig.java
@@ -9,6 +9,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
      * 解决跨域请求的问题
+     * allowedOrigins：允许哪些源网站访问
+     * allowCredentials：是否浏览器应该发送credentials，例如cookies Access-Control-Allow-Credentials
+     * allowedMethods：允许何种方式访问
+     * maxAge：设置等待时间
      * @param registry
      */
     @Override
