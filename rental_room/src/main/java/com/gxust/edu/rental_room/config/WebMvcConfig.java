6db1e399279ec463c211781182127887d00b6a37
shicaihua <2502993659@qq.com>
2020-01-13 11:40:30 +0800

    解决跨域请求问题

---
+++ rental_room/src/main/java/com/gxust/edu/rental_room/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.gxust.edu.rental_room.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 解决跨域请求的问题
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}
