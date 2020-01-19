e6ec568c16a8293675419ba50bd49fa8ff7a1e84
shicaihua <2502993659@qq.com>
2020-01-19 10:15:07 +0800

    权限模块

---
+++ rental_room/src/main/java/com/gxust/edu/rental_room/controller/PermissionController.java
@@ -0,0 +1,105 @@
+package com.gxust.edu.rental_room.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.gxust.edu.rental_room.domain.Permission;
+import com.gxust.edu.rental_room.query.PermissionQuery;
+import com.gxust.edu.rental_room.service.impl.PermissionServiceImpl;
+import com.gxust.edu.rental_room.utils.JsonModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/permission")
+public class PermissionController {
+
+    @Autowired
+    PermissionServiceImpl permissionService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public JsonModel addPermission(Permission permission) {
+        JsonModel jsonModel = new JsonModel();
+        boolean result = permissionService.add(permission);
+        if (result) {
+            jsonModel.setMsg("添加成功");
+            jsonModel.setSuccess(true);
+        } else {
+            jsonModel.setMsg("添加失败");
+            jsonModel.setSuccess(false);
+        }
+        return jsonModel;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    @ResponseBody
+    public JsonModel deletePermission(String id) {
+        JsonModel jsonModel = new JsonModel();
+        String[] idsStr = id.split(",");
+        int[] ids = new int[idsStr.length];
+        for (int i = 0; i < idsStr.length; i++) {
+            ids[i] = Integer.parseInt(idsStr[i]);
+        }
+        boolean result = permissionService.deleteByIds(ids);
+        if (result) {
+            jsonModel.setMsg("删除成功");
+            jsonModel.setSuccess(true);
+        } else {
+            jsonModel.setMsg("删除失败");
+            jsonModel.setSuccess(false);
+        }
+        return jsonModel;
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @ResponseBody
+    public JsonModel update(Permission permission) {
+        JsonModel jsonModel = new JsonModel();
+        boolean result = permissionService.update(permission);
+        if (result) {
+            jsonModel.setMsg("更新成功");
+            jsonModel.setSuccess(true);
+        } else {
+            jsonModel.setMsg("更新失败");
+            jsonModel.setSuccess(false);
+        }
+        return jsonModel;
+    }
+
+    @RequestMapping(value = "/findById", method = RequestMethod.GET)
+    @ResponseBody
+    public JsonModel findById(Integer id) {
+        JsonModel jsonModel = new JsonModel();
+        Permission permission = permissionService.findById(id);
+        if (permission != null) {
+            jsonModel.setMsg("根据ID查找成功");
+            jsonModel.setSuccess(true);
+            jsonModel.setData(permission);
+        } else {
+            jsonModel.setMsg("根据ID查找失败");
+            jsonModel.setSuccess(false);
+        }
+        return jsonModel;
+    }
+
+    @RequestMapping(value = "/findByQuery", method = RequestMethod.GET)
+    @ResponseBody
+    public JsonModel findByQuery(PermissionQuery permissionQuery) {
+        JsonModel jsonModel = new JsonModel();
+        PageInfo<Permission> pageInfo = permissionService.findByQuery(permissionQuery);
+        List<Permission> permissionList = pageInfo.getList();
+        if (permissionList != null && permissionList.size() >= 0) {
+            jsonModel.setMsg("查找成功");
+            jsonModel.setSuccess(true);
+            jsonModel.setData(permissionList);
+        } else {
+            jsonModel.setMsg("查找失败");
+            jsonModel.setSuccess(false);
+        }
+        return jsonModel;
+    }
+}
